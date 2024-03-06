7c9f560f2b97c85a43634b6acb0167a3558f73ee
rubng87 <gallegoruben_srye@hotmail.com>
2024-03-06 13:48:31 +0100

    reserva_hotel

---
+++ js/reserva_hotel.js
@@ -0,0 +1,9 @@
+alert("hola")
+
+function reservar () {
+    let nombre = document.getElementById(nombre).value;
+    let apellido = document.getElementById(apellido).value;
+    let numeroAdultos = document.getElementById(numeroAdultos).value;
+    let numeroNiños = document.getElementById(numeroNiños).value
+    let comida = document.getElementById()
+}
