637cfc5344197c40a85f4eea7004b374eab3b66b
DESKTOP-J452Q1K\galle <gallegoruben_srye@hotmail.com>
2024-03-07 19:26:46 +0100

    Reserva Hotel

--- js/reserva_hotel.js
+++ js/reserva_hotel.js
@@ -4,23 +4,26 @@ function reservar () {
     let apellido = document.getElementById("apellido").value;
     let numeroAdultos = document.getElementById("numeroAdultos").value;
     let numeroNiños = document.getElementById("numeroNiños").value
-
-
     let opcionesComida = document.getElementsByName("operador")
 
     let regimen = ""
 
-    console.log(opcionesComida);
+    for (let i = 0; i < opcionesComida.length; i++) {
+        if (opcionesComida[i].checked == true) {
+            regimen = opcionesComida[i].value
+            break
+        } 
+    }
 
+    alert(`
 
-    // for (let i = 0; i < opcionesComida.length; i++) {
-    //     if (opcionesComida[i].checked ) {
-    //         regimen = opcionesComida[i].value
-    //         break
-    //     } 
-    // }
-
-
+    Datos de la reserva:
+    Nombre: ${nombre}
+    Apellido: ${apellido}
+    Adultos: ${numeroAdultos}
+    Niños: ${numeroNiños}
+    Comidas: ${regimen}
+    `)
     // console.log(opcionesComida[0].value);
 
 
